a379be523c555b17ebe3206f130c9a50d34fa50b
Michalina Zeligowska <michalina.zeligowska@gmail.com>
2018-11-24 09:44:12 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// function ask() {
+//     const confirmation = confirm('wanna question?');
+//     if(confirmation) {
+//         var answer = prompt('how you doin?');
+//         answer += ' ' + prompt('still?');
+//         answer += ' ' + prompt('and now?');
+//         alert(answer);
+//     }
+// }
+
+// ask();
+
+
+open(prompt('podaj adres'));
