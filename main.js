ef3cb96d39c480c01c767bd80fe99dd1d217366b
Michalina Zeligowska <michalina.zeligowska@gmail.com>
2018-11-24 13:58:11 +0100

    zadanie ostatnie

--- main.js
+++ main.js
@@ -3,19 +3,34 @@ const btn = document.querySelector('.btn');
 btn.setAttribute('disabled', '');
 btn.setAttribute('id', 'button');
 btn.removeAttribute('disabled');
+btn.style.cssText = `
+    border-radius: 5px;
+    color: white
+`;
 
-const colors = ['red', 'blue', 'green', 'yellow', 'white'];
+const colors = ['red', 'blue', 'green', 'yellow', 'white', 'pink'];
 
-var i = 0;
-
-function changeColors() {
-    btn.style.background = colors[i];
-    i++;
-    if(i === colors.length)
-        i = 0;
-}
+let i = 0;
 
 btn.addEventListener(
     'click',
-    changeColors
+    () => {
+        btn.style.background = colors[i++];
+        if(i === colors.length)
+            i = 0;
+        alert(input.value);
+    }
 );
+
+const h1 = document.createElement('h1');
+const input = document.createElement('input');
+
+document.body.appendChild(h1);
+document.body.appendChild(input);
+
+input.addEventListener(
+    'input',
+    () => {
+        h1.innerText = input.value
+    }
+)
